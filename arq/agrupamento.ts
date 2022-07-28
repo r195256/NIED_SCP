bb2679fd8c7315724ef9562bebcaa782e1c02527
Rennan <r195256@dac.unicamp.br>
2022-07-28 15:57:47 -0300

    Adicionando o arquivo de sugestao de agrupamento de campos

---
+++ arq/agrupamento.ts
@@ -0,0 +1,30 @@
+Informaçoes gerais
+-Categoria;
+-Modelo;
+-Marca;
+-Ano;
+-Outros;
+
+Informações específicas
+-Processador;
+-Memória;
+-Disco;
+-Monitor;
+-Sistema operacional;
+
+Informações do produto
+-Garantia;
+-Part Number;
+-Serial Number;
+-Tag Service;
+-Adicional;
+
+Informações do projeto
+-Projeto;
+-Financiador;
+-Loja;
+-Nota Fiscal;
+-Data;
+-Quantidade;
+-Patrimônio.
+
